b254bfbe1acb5f57aee8aad9e0acfecd03d4de35
Matias Lotito <matiaslotitoralli@gmail.com>
2026-01-09 00:11:47 +0100

    feat(test): added integration test class with test containers

---
+++ src/test/kotlin/com/matias/timetracking/helper/IntegrationTest.kt
@@ -0,0 +1,28 @@
+package com.matias.timetracking.helper
+
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+
+@Testcontainers
+class IntegrationTest {
+
+    companion object {
+        @Container
+        val postgres = PostgreSQLContainer(DockerImageName.parse("postgres:16")).apply {
+            withDatabaseName("postgres")
+            withUsername("test")
+            withPassword("test")
+        }
+    }
+
+    @DynamicPropertySource
+    fun registerProps(registry: DynamicPropertyRegistry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl)
+        registry.add("spring.datasource.username", postgres::getUsername)
+        registry.add("spring.datasource.password", postgres::getPassword)
+    }
+}
