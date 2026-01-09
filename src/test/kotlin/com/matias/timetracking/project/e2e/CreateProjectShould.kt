4cfc1a7ea2a9de5a0ee993c2d0a1cdc62371a3b5
Matias Lotito <matiaslotitoralli@gmail.com>
2026-01-09 01:05:32 +0100

    feat(project): optimized imports

--- src/test/kotlin/com/matias/timetracking/project/e2e/CreateProjectShould.kt
+++ src/test/kotlin/com/matias/timetracking/project/e2e/CreateProjectShould.kt
@@ -1,18 +1,13 @@
 package com.matias.timetracking.project.e2e
 
 import com.matias.timetracking.helper.IntegrationTest
-import com.matias.timetracking.project.domain.aggregate.Project
 import com.matias.timetracking.project.infrastructure.controller.create.dto.CreateProjectRequest
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.resttestclient.TestRestTemplate
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient
-import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate
-import org.springframework.boot.resttestclient.postForEntity
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
-import org.springframework.http.HttpStatus
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.test.web.servlet.client.RestTestClient
 
