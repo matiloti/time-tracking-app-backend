0563d7fa1761c9b2c40164f9e88246411bfe7a09
Matias <matiaslotitoralli@gmail.com>
2026-02-13 17:50:25 +0100

    fix(tasks): fix priority ID domain issue

--- src/main/kotlin/com/matias/timetracking/project/domain/enums/Priority.kt
+++ src/main/kotlin/com/matias/timetracking/project/domain/enums/Priority.kt
@@ -2,9 +2,9 @@ package com.matias.timetracking.project.domain.enums
 
 enum class Priority(val id: Int) {
     INVALID(-1),
-    LOW(0), // TODO: fix priority, the first ID in DB is 1
-    MEDIUM(1),
-    HIGH(2),
+    LOW(1),
+    MEDIUM(2),
+    HIGH(3),
     ;
 
     fun isInvalid() = this == INVALID
